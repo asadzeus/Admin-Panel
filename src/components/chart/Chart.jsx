62e7efc95cedc954484287d8ed8096636378e051
Alper <fod.zeus1@gmail.com>
2024-06-07 14:34:43 +0300

    chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,9 +1,53 @@
-import './chart.css'
+import "./chart.css";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {name: "January", Total: 1200},
+  {name: "February", Total: 2100},
+  {name: "March", Total: 900},
+  {name: "April", Total: 1200},
+  {name: "May", Total: 2500},
+  {name: "June", Total: 1400},
+];
 
 export default function Chart() {
   return (
-    <div className='chart'>
-      chart
+    
+    <div className="chart">
+      <div className="ctitle">Last 6 months</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name"  stroke="rgb(128,128,128)"/>
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
-  )
+  );
 }
